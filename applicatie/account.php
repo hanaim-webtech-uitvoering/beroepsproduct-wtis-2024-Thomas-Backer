d65ef3ca3efa9a68dce5a98a1b149a7170b42e70
Thomas-Backer <T.Backer@student.han.nl>
2025-01-15 19:33:05 +0100

    Update account pagina
    Kleine style gegeven

--- applicatie/account.php
+++ applicatie/account.php
@@ -12,9 +12,10 @@ if (!isset($_SESSION['username'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Account</title>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picnic">
+    <title>Account</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
     <h2>Welkom, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h2>
